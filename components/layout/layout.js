4e39a9faac1c093091e3b6a16d2319a776a9bfe5
linear-digital <tamizuddin.tr@gmail.com>
2024-10-21 21:31:07 +0600

    first commit

---
+++ components/layout/layout.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Bottom from "./bottom";
+import Search from "../elements/search";
+import Footer from "./footer";
+import Header from "./header";
+import Sidebar from "./sidebar";
+import BackToTop from "../elements/backToTop";
+import Head from "next/head";
+
+const Layout = ({ children }) => {
+    const addClass = () => {
+        document.body.classList.add("canvas-opened");
+    };
+
+    const removeClass = () => {
+        document.body.classList.remove("canvas-opened");
+    };
+
+    const openSearch = () => {
+        document.body.classList.toggle("open-search-form");
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Stories - Personal Blog HTML Template</title>
+                <meta property="og:title" content="My page title" key="title" />
+                {/* <link href="" rel="stylesheet" /> */}
+            </Head>
+            <Sidebar removeClass={removeClass} />
+            <Header addClass={addClass} openSearch={openSearch} />
+            <Search />
+
+            {children}
+
+            <Bottom />
+            <Footer removeClass={removeClass} />
+            <BackToTop />
+        </>
+    );
+};
+
+export default Layout;
