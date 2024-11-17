1005e92e9bb54dc78d010f2e01890b81424fe79f
MD TAMIZ UDDIN <tamizrabbi@gmail.com>
2024-11-18 00:55:22 +0600

    update pages/index.js

---
+++ components/MostPopularPosts.js
@@ -0,0 +1,73 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import api from './axios.instance';
+import Link from 'next/link';
+import moment from 'moment';
+import { Spin } from 'antd';
+
+const MostPopularPosts = () => {
+    const { data: post, isLoading } = useQuery({
+        queryKey: ['popular-post'],
+        queryFn: async () => {
+            const res = await api.get('/blog?sort=true&limit=5')
+            return res.data
+        }
+    })
+
+    if (isLoading) {
+        return <Spin size='large' />
+    }
+    return (
+        <div className="sidebar-widget widget-latest-posts mb-50 wow fadeInUp animated">
+            <div className="widget-header-1 position-relative mb-30">
+                <h5 className="mt-5 mb-30">
+                    Most popular
+                </h5>
+            </div>
+            <div className="post-block-list post-module-1">
+                <ul className="list-post">
+                    {post?.map((item, i) => (
+                        <li
+                            key={i}
+                            className="mb-30 wow fadeInUp animated">
+                            <div className="d-flex bg-white has-border p-25 hover-up transition-normal border-radius-5">
+                                <div className="post-content media-body">
+                                    <h6 className="post-title mb-15 text-limit-2-row font-medium">
+                                        <Link href={`/blog/${item?._id}`}>
+                                            <a>
+                                                {item?.title}
+                                            </a>
+                                        </Link>
+                                    </h6>
+                                    <div className="entry-meta meta-1 float-left font-x-small text-uppercase">
+                                        <span className="post-on">
+                                            {moment(item?.createdAt).fromNow()}
+                                        </span>
+                                        <span className="post-by has-dot">
+                                            {item?.views} views
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="post-thumb post-thumb-80 d-flex ml-15 border-radius-5 img-hover-scale overflow-hidden">
+                                    <Link href={`/blog/${item?._id}`}>
+                                        <a
+                                            className="color-white"
+
+                                        >
+                                            <img
+                                                src={`${item?.image}`}
+                                                alt=""
+                                            />
+                                        </a>
+                                    </Link>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default MostPopularPosts;
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import api from "../components/axios.instance";
 import moment from "moment";
 import BlogCard from "./blog/BlogCard";
 import SubscribeForm from "../components/SubscribeForm";
+import MostPopularPosts from "../components/MostPopularPosts";
 
 export async function getServerSideProps() {
     // Fetch data from external API
@@ -362,55 +363,8 @@ function Home({ posts, tags, letest }) {
                                                 </div>
                                             ))}
                                         </div>
-                                        <div className="sidebar-widget widget-latest-posts mb-50 wow fadeInUp animated">
-                                            <div className="widget-header-1 position-relative mb-30">
-                                                <h5 className="mt-5 mb-30">
-                                                    Most popular
-                                                </h5>
-                                            </div>
-                                            <div className="post-block-list post-module-1">
-                                                <ul className="list-post">
-                                                    {post.slice(1, 5).map((item, i) => (
-                                                        <li
-                                                            key={i}
-                                                            className="mb-30 wow fadeInUp animated">
-                                                            <div className="d-flex bg-white has-border p-25 hover-up transition-normal border-radius-5">
-                                                                <div className="post-content media-body">
-                                                                    <h6 className="post-title mb-15 text-limit-2-row font-medium">
-                                                                        <Link href={`/blog/${item?.id}`}>
-                                                                            <a>
-                                                                                {item?.title}
-                                                                            </a>
-                                                                        </Link>
-                                                                    </h6>
-                                                                    <div className="entry-meta meta-1 float-left font-x-small text-uppercase">
-                                                                        <span className="post-on">
-                                                                            {item?.date}
-                                                                        </span>
-                                                                        <span className="post-by has-dot">
-                                                                            {item?.views} views
-                                                                        </span>
-                                                                    </div>
-                                                                </div>
-                                                                <div className="post-thumb post-thumb-80 d-flex ml-15 border-radius-5 img-hover-scale overflow-hidden">
-                                                                    <Link href={`/blog/${item?.id}`}>
-                                                                        <a
-                                                                            className="color-white"
-
-                                                                        >
-                                                                            <img
-                                                                                src={`/assets/imgs/news/${item?.img}`}
-                                                                                alt=""
-                                                                            />
-                                                                        </a>
-                                                                    </Link>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                    ))}
-                                                </ul>
-                                            </div>
-                                        </div>
+                                        {/* Most popular */}
+                                       <MostPopularPosts />
                                         <div className="sidebar-widget widget-latest-posts mb-50 wow fadeInUp animated">
                                             <div className="widget-header-1 position-relative mb-30">
                                                 <h5 className="mt-5 mb-30">
@@ -539,7 +493,7 @@ function Home({ posts, tags, letest }) {
                                                 </ul>
                                             </div>
                                         </div>
-                                        <div className="sidebar-widget widget_instagram wow fadeInUp animated">
+                                        {/* <div className="sidebar-widget widget_instagram wow fadeInUp animated">
                                             <div className="widget-header-1 position-relative mb-30">
                                                 <h5 className="mt-5 mb-30">
                                                     Instagram
@@ -651,7 +605,7 @@ function Home({ posts, tags, letest }) {
                                                     </li>
                                                 </ul>
                                             </div>
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </div>
